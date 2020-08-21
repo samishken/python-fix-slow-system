b3d6bb95159bfb021cebf903d42024d6e73e74cf
Samuel Haile <samishken@gmail.com>
2020-08-21 00:26:41 -0400

    create dailysync.py

---
+++ dailysync.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import subprocess
+from multiprocessing import Pool
+import re
+import os
+
+src = "~/data/prod/"
+
+def run(directory):
+    src = directory
+    dest = re.sub("(/prod)", r"\1"+"_backup",src)
+    print("Moving Data from {} to {}".format(src,dest))
+    subprocess.call(["rsync", "-arq", src, dest])
+    
+
+if __name__ == "__main__":
+  dir_list = []
+  for root, dirs, files in os.walk(src, topdown=False):
+      for name in dirs:
+          dir_list.append(os.path.join(root, name))
+  p = Pool(len(dir_list))
+  p.map(run, dir_list)
+  
